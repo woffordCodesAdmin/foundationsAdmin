7b164bed911b6dda5d4fa04b7cb6acc9548cf271
Jordan M. Wofford <wofford.codes@gmail.com>
2021-07-08 12:29:11 -0400

    CommentBoard:

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_074746) do
+ActiveRecord::Schema.define(version: 2021_07_08_154019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "daily_vids", force: :cascade do |t|
     t.string "title"
